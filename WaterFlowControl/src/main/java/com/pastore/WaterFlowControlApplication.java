fe4fcfd6a0614473e7a7959b6581226f2d0aa080
Andrea Pastore <andrea.pastore90@outlook.com>
2019-12-24 20:25:23 +0100

    progetto inizializzato

---
+++ WaterFlowControl/src/main/java/com/pastore/WaterFlowControlApplication.java
@@ -0,0 +1,13 @@
+package com.pastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterFlowControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterFlowControlApplication.class, args);
+	}
+
+}
