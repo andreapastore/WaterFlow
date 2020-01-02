6c23dc04a6504d122cec8ee0e4defa2a59fb93a0
Andrea Pastore <andrea.pastore90@outlook.com>
2020-01-02 16:01:45 +0100

    Sicurezza disattivata, dobbiamo saperne di più prima di metterci mano, inoltre il progetto deve essere finito

--- WaterFlowControl/src/main/java/com/pastore/restController/RestLogin.java
+++ WaterFlowControl/src/main/java/com/pastore/restController/RestLogin.java
@@ -1,30 +1,53 @@
 package com.pastore.restController;
 
 import java.security.Principal;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pastore.entity.Socio;
+import com.pastore.service.SocioService;
 
 @RestController
 @RequestMapping(value = "/api")
 public class RestLogin 
 {
+	@Autowired
+	private SocioService socioService;
+	
+	//@Autowired
+	//private WebSecurityConfig webSecurityConfig;
+	
 	@GetMapping(value = "/login", produces = "application/json")
 	public ResponseEntity<Socio> socioLogin(Principal principal)
 	{
-		System.out.println("SONO NELLA LOGIN");
 		try 
 		{
-			//principal.
+			String user = "user";
+			Optional<Socio> socio = socioService.ricercaSocioByUsername(user);
+			if (!socio.isEmpty())
+			{
+				System.out.println("ho trovato il socio");
+				//webSecurityConfig.salvaInConfigurazione(socio.get().getUsername(), socio.get().getPassword());
+				return new ResponseEntity<>(socio.get(), HttpStatus.OK);
+			}
+			else
+			{
+				System.out.println("non ho trovato il socio");
+				return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			}
+			
+			
 		} 
 		catch (Exception e) 
 		{
-			
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		return null;
 	}
 }
--- WaterFlowControl/src/main/java/com/pastore/security/WebSecurityConfig.java
+++ WaterFlowControl/src/main/java/com/pastore/security/WebSecurityConfig.java
@@ -1,24 +1,55 @@
-package com.pastore.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter
-{
-
-		@Override
-        protected void configure(HttpSecurity http) throws Exception {
-                http.authorizeRequests().antMatchers("/**").hasRole("USER").and().formLogin();
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-                auth.inMemoryAuthentication().withUser("user2").password("{noop}password").roles("USER");
-        }
-	
-}
+//package com.pastore.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+//{
+//	@Autowired
+//	private AuthenticationManagerBuilder auth;
+//	
+//		@Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//                //http.authorizeRequests().antMatchers("/**").hasRole("USER").and().formLogin();
+//			http.authorizeRequests()
+//	        .antMatchers("/**").permitAll()
+//	        .anyRequest().authenticated()
+//	        .and()
+//	      .formLogin();
+//			/*
+//	        .loginPage("/login")
+//	        .permitAll()
+//	        .and()
+//	      .logout()
+//	        .permitAll();
+//	        */
+//        }
+//		/*
+//        @Override
+//        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//                auth.inMemoryAuthentication().withUser("user2").password("{noop}password").roles("USER");
+//        }
+//        */
+//        public void salvaInConfigurazione(String user, String password) throws Exception
+//        {
+//        	auth.inMemoryAuthentication().withUser(user).password("{noop}" + password).roles("USER");
+//        }
+//        /*
+//        private void configuraUsers(AuthenticationManagerBuilder auth, String user, String password)
+//        {
+//        	try {
+//				auth.inMemoryAuthentication().withUser(user).password("{noop}" + password).roles("USER");
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//        }
+//        */
+//	
+//}
